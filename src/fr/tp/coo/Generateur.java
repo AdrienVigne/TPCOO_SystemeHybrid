1def0bd58f707a2a5c7203cd75f4b6013e38d452
halla <hallakmaher186@gmail.com>
2021-09-14 10:58:37 +0200

    ajout Processeur et generateur

---
+++ src/fr/tp/coo/Generateur.java
@@ -0,0 +1,32 @@
+package fr.tp.coo;
+
+public class Generateur extends Block {
+    Etat e;
+    e.nom="s";
+    e.
+    @Override
+    public void externe(Sortie s) {
+
+
+    }
+
+    @Override
+    public void interne(Entree e ) {
+        if()
+    }
+
+    @Override
+    public double avancement() {
+        return 0;
+    }
+
+    @Override
+    public void sortie() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
---
+++ src/fr/tp/coo/Processeur.java
@@ -0,0 +1,28 @@
+package fr.tp.coo;
+
+public class Processeur extends Block{
+    @Override
+    public void externe(Sortie s) {
+
+    }
+
+    @Override
+    public void interne(Entree e) {
+
+    }
+
+    @Override
+    public double avancement() {
+        return 0;
+    }
+
+    @Override
+    public void sortie() {
+
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
